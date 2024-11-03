7b81ebd17eb37a43cddc634f56fa581149248dfe
Nyambura765 <nyamburafavour765@gmail.com>
2024-11-03 07:47:29 +0300

    Initial commit

---
+++ contracts/1_Storage.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+//primitive data types
+//boolean true or false
+//uint256
+
+contract Token2024 {
+    bool public boo = true;
+}
